c54fc620e2605068e1aac6c1717868f143a659eb
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2011-09-02 12:23:40 +0100

    Util: add some comments.

--- src/Util/Prelude.hs
+++ src/Util/Prelude.hs
@@ -5,18 +5,24 @@ import Prelude
 import System.Directory
 import System.FilePath
 
+-- | Given two orderings, returns the second if the first is 
+-- `EQ` and returns the first otherwise.
 thenCmp :: Ordering -> Ordering -> Ordering
 thenCmp EQ x = x
 thenCmp x _ = x
 
+-- | Given a file path, if the first character is a ~ then
+-- expands the ~ to the users' home directory.
 expandPathIO :: String -> IO String
 expandPathIO ('~':'/':d) = do
     tilde <- getHomeDirectory -- will fail if HOME not defined
     return $ joinPath [tilde, d]
 expandPathIO other = return other
 
+-- | Remove whitespace from the beginning and end of a string.
 trim :: String -> String
 trim = f . f where f = reverse . dropWhile isSpace
 
+-- | Compute the cartesian product of a list of lists.
 cartProduct :: [[a]] -> [[a]]
 cartProduct = sequence
