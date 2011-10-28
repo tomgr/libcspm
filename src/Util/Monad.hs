32cdf4d469f685d8e7293aa645a6029b17b4e674
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2011-10-28 17:31:50 +0100

    Util.Monad: add andM and orM.

--- src/Util/Monad.hs
+++ src/Util/Monad.hs
@@ -1,6 +1,23 @@
-module Util.Monad where
+-- | Misc utility functions that are defined on monads.
+module Util.Monad (
+    concatMapM,
+    andM,
+    orM
+) where
 
 import Control.Monad
 
 concatMapM :: (Monad m) => (a -> m [b]) -> [a] -> m [b]
 concatMapM f xs = liftM concat (mapM f xs)
+
+andM :: (Monad m) => [m Bool] -> m Bool
+andM [m] = m
+andM (m:ms) = do
+    b <- m
+    if b then andM ms else return False
+
+orM :: (Monad m) => [m Bool] -> m Bool
+orM [m] = m
+orM (m:ms) = do
+    b <- m
+    if not b then orM ms else return True
