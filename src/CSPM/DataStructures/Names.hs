7312c8bf0285880f846fd01b2898cf336f5c7955
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2011-12-20 17:28:00 +0000

    Change how names are represented.
    There are now two types: UnRenamedNames which only exist prior to renaming,
    and Names, which exist only after renaming and are guaranteed to be unique.
    
    In order to generate unique names we make use of the Data.Supply module.

--- src/CSPM/DataStructures/Names.hs
+++ src/CSPM/DataStructures/Names.hs
@@ -1,19 +1,119 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-module CSPM.DataStructures.Names where
+-- | Names used by the evaluator. This is heavily inspired by GHC.
+module CSPM.DataStructures.Names (
+    OccName(..),
+    UnRenamedName(..),
+    Name(..),
+    NameType,
+
+    mkExternalName, mkInternalName, mkWiredInName,
+    isNameDataConstructor,
+) where
+
+import Control.Monad.Trans
+import Data.IORef
+import Data.Supply
 import Data.Typeable
+import System.IO.Unsafe
 
-data Name = 
-    Name String
-    | InternalName String
-    deriving (Eq, Ord, Typeable, Show)
+import Util.Annotated
+import Util.PrettyPrint
 
-isInternal :: Name -> Bool
-isInternal (Name _) = False
-isInternal (InternalName _) = True
+-- | A name that occurs in the source code somewhere.
+data OccName = 
+    OccName String
+    deriving (Eq, Ord, Show, Typeable)
 
-mkInternalName :: String -> Name
-mkInternalName = InternalName
+instance PrettyPrintable OccName where
+    prettyPrint (OccName s) = text s
 
-data QualifiedName =
-    UnQual Name
-    deriving (Eq, Show)
+-- | A name that has not yet been renamed. Created by the parser.
+data UnRenamedName =
+    UnQual OccName
+    deriving (Eq, Ord, Show, Typeable)
+
+instance PrettyPrintable UnRenamedName where
+    prettyPrint (UnQual n) = prettyPrint n
+
+-- | A renamed name and is the exclusive type used after the renamer.
+data Name =
+    Name {
+        -- | The type of this name.
+        nameType :: NameType,
+        -- | The original occurence of this name (used for error messages).
+        nameOccurrence :: !OccName,
+        -- | Where this name was defined. If this occurs in a pattern, then it
+        -- will be equal to the location of the pattern, otherwise it will be
+        -- equal to the location of the definition that this name binds to.
+        nameDefinition :: !SrcSpan,
+        -- | The unique identifier for this name. Inserted by the renamer.
+        nameUnique :: !Int,
+        -- | Is this name a type constructor, i.e. a datatype or a channel?
+        -- Only External names can be.
+        nameIsConstructor :: Bool
+    }
+    deriving Typeable
+
+data NameType =
+    -- | An externally visible name (like a top level definition).
+    External
+    -- | A name created by the renamer, but from the users' source (e.g. from
+    -- a lambda).
+    | Internal
+    -- | A built in name.
+    | WiredIn
+
+instance Eq Name where
+    n1 == n2 = nameUnique n1 == nameUnique n2
+
+instance Ord Name where
+    compare n1 n2 = compare (nameUnique n1) (nameUnique n2)
+
+instance PrettyPrintable Name where
+    prettyPrint n = prettyPrint (nameOccurrence n)
+        --case nameType n of
+        --    External -> text "E"
+        --    Internal -> text "I"
+        --    WiredIn -> text "WI"
+        -- <> text (show (nameUnique n))
+        -- <> parens (prettyPrint (nameOccurrence n))
+                    
+        --case nameType n of
+        --    External -> text ('v':show (nameUnique n))
+        --                <> parens (prettyPrint (nameOccurrence n))
+        --    Internal ->
+        --    WiredIn ->
+
+instance Show Name where
+    show n = show (prettyPrint n)
+
+nameUniqueSupply :: IORef (Supply Int)
+nameUniqueSupply = unsafePerformIO (do
+    s <- newNumSupply
+    newIORef s)
+
+takeNameUnique :: MonadIO m => m Int
+takeNameUnique = do
+    s <- liftIO $ readIORef nameUniqueSupply
+    let (s1, s2) = split2 s
+    liftIO $ writeIORef nameUniqueSupply s2
+    return $ supplyValue s1
+
+mkExternalName :: MonadIO m => OccName -> SrcSpan -> Bool -> m Name
+mkExternalName o s b = do
+    u <- takeNameUnique
+    return $ Name External o s u b
+
+mkInternalName :: MonadIO m => OccName -> SrcSpan -> m Name
+mkInternalName o s = do
+    u <- takeNameUnique
+    return $ Name Internal o s u False
+
+mkWiredInName :: MonadIO m => OccName -> m Name
+mkWiredInName o = do
+    u <- takeNameUnique
+    return $ Name WiredIn o Unknown u False
+
+-- | Does the given Name correspond to a data type or a channel definition.
+isNameDataConstructor :: Name -> Bool
+isNameDataConstructor n = nameIsConstructor n
