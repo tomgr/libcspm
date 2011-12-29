ac895dfe4fbe72ca6297cad8791480d9961b4a44
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2011-12-29 23:28:51 +0000

    Util: add replaceLast that replaces the last item in a list.

--- src/Util/List.hs
+++ src/Util/List.hs
@@ -5,3 +5,8 @@ import Data.List
 -- | Returns true iff the list has no duplicates.
 noDups :: Eq a => [a] -> Bool
 noDups xs = nub xs == xs
+
+-- | Replaces the last item in a list. Assumes the list is non empty.
+replaceLast :: [a] -> a -> [a]
+replaceLast [_] v = [v]
+replaceLast (x:xs) v = replaceLast xs v
