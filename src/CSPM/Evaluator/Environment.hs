3709208e74add78c4258b1a1c9b5fe5520fb0a0f
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2011-12-29 12:08:29 +0000

    Make the environment a flat map.
    The renamer ensures uniqueness.

--- src/CSPM/Evaluator/Environment.hs
+++ src/CSPM/Evaluator/Environment.hs
@@ -1,13 +1,26 @@
 module CSPM.Evaluator.Environment (
-    module Util.HierarchicalMap,
     Environment,
+    new, lookup, newLayerAndBind, toList,
 ) where
 
 import qualified Data.Map as M
-import Prelude
+import Prelude hiding (lookup)
 
 import CSPM.DataStructures.Names
 import {-# SOURCE #-} CSPM.Evaluator.Values
-import Util.HierarchicalMap
+import Util.Exception
 
-type Environment = HierarchicalMap Name Value
+type Environment = M.Map Name Value
+
+new :: Environment
+new = M.empty
+
+toList :: Environment -> [(Name, Value)]
+toList = M.toList
+
+lookup :: Environment -> Name -> Value
+lookup env n = M.findWithDefault (panic "lookup not found") n env
+
+newLayerAndBind :: Environment -> [(Name, Value)] -> Environment
+newLayerAndBind env nvs = 
+    foldr (\ (n,v) env -> M.insert n v env) env nvs
