665b34a84a51234611bce49a04b958da2d2c94d2
Thomas Gibson-Robinson <thomas.gibsonrobinson@gmail.com>
2012-04-20 09:14:01 +0100

    Use the reader monad, rather than a custom one.

--- src/CSPM/Evaluator/Monad.hs
+++ src/CSPM/Evaluator/Monad.hs
@@ -1,6 +1,6 @@
 module CSPM.Evaluator.Monad where
 
---import Control.Monad.State
+import Control.Monad.Reader
 import Prelude hiding (lookup)
 
 import CSPM.Compiler.Processes
@@ -14,47 +14,33 @@ data EvaluationState =
         environment :: Environment,
         parentProcName :: Maybe ProcName
     }
-    
-type EvaluationMonad = LazyEvalMonad EvaluationState
+  
+type EvaluationMonad = Reader EvaluationState
 
-newtype LazyEvalMonad s a = LazyEvalMonad { 
-    -- Notice that this doesn't yield a new environment
-    unLazyEvalMonad :: s -> a
-}
-        
-runLazyEvalMonad :: s -> LazyEvalMonad s a -> a
-runLazyEvalMonad st (LazyEvalMonad prog) = prog st
+gets :: (EvaluationState -> a) -> EvaluationMonad a
+gets = asks
 
-gets :: (s -> a) -> LazyEvalMonad s a
-gets f = LazyEvalMonad (\st -> f st)
-
-modify :: (s -> s) -> LazyEvalMonad s a -> LazyEvalMonad s a
-modify f prog = LazyEvalMonad (\st -> unLazyEvalMonad prog (f st))
-
-instance Monad (LazyEvalMonad a) where
-    (LazyEvalMonad p1) >>= f = 
-        LazyEvalMonad (\ st -> 
-            let
-                a = p1 st
-                LazyEvalMonad p2 = f a
-            in p2 st)
-    return k = LazyEvalMonad (\ st -> k)
+modify :: (EvaluationState -> EvaluationState) -> EvaluationMonad a -> EvaluationMonad a
+modify = local
 
 runEvaluator :: EvaluationState -> EvaluationMonad a -> a
-runEvaluator st prog = runLazyEvalMonad st prog
+runEvaluator st prog = runReader prog st
 
 getState :: EvaluationMonad EvaluationState
 getState = gets id
+{-# INLINE getState #-}
 
 getEnvironment :: EvaluationMonad Environment
 getEnvironment = gets environment
+{-# INLINE getEnvironment #-}
 
-lookupVar :: Name -> EvaluationMonad Value
-lookupVar n = do
+lookupVarMaybeThunk :: Name -> EvaluationMonad Value
+lookupVarMaybeThunk n = do
     -- This should never produce an error as the TC would
     -- catch it
     env <- getEnvironment
     return $ lookup env n
+{-# INLINE lookupVarMaybeThunk #-}
 
 -- | Implements non-recursive lets.
 addScopeAndBind :: [(Name, Value)] -> EvaluationMonad a -> EvaluationMonad a
